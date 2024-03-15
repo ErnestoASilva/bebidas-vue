3b356712a79ea9fb542aa1b7ec46f39e898014ac
Ernesto Silva <silvaochoa55@gmail.com>
2024-03-14 17:22:34 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Header.vue'
+import Modal from './components/Modal.vue'
+import Notificacion from './components/Notificacion.vue'
+
+</script>
+
+<template>
+  <Header />
+
+  <main class="container mx-auto mt-10">
+    <RouterView />
+  </main>
+
+  <Modal/>
+  <Notificacion/>
+  
+</template>
+
+<style scoped>
+
+</style>
